59e93f49ccf09c6decd7577409e6571ec8a10953
André Echevarria <echevarriandre@gmail.com>
2022-06-25 15:35:44 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex justify-center mt-10">
+    <h1 class="text-xl text-red-400">Vue 3 + Tailwind template</h1>
+  </main>
+</template>
